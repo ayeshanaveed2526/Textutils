6c01717b46e13693cec4e9252756b20b57ec2cc5
Ayesha Naveed <hr34772@gmail.com>
2025-06-27 20:04:09 +0500

    DARKMODE

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 
 import "./App.css";
 import Navbar from "./Navbar";
-// import  FormText from "./FormText";
-import About from "./About";
+import  FormText from "./FormText";
+// import About from "./About";
+import React,{ useState} from "react";
 
 
 function App() {
+  const [mode, setMode] = useState('light'); // 'light' or 'dark'
+  const toggleBtn = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.background='#343a40';
+    } else {
+      setMode('light');
+      document.body.style.background='white';
+    }
+  };
+
   return (
     <>
- <Navbar/>
- {/* <div className="container my-3">
-  <FormText heading="Enter the text to analyze below" />
-
- </div> */}
-<About/>
+      <Navbar mode={mode} toggleBtn={toggleBtn} />
+      <div className="container my-3">
+        <FormText heading="Enter the text to analyze below"  mode={mode} toggleBtn={toggleBtn}/>
+      </div>
+      {/* <About/> */}
     </>
   );
 }
--- src/FormText.jsx
+++ src/FormText.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function FormText({ heading }) {
+export default function FormText({ heading,mode}) {
   const [isItalic, setIsItalic] = useState(false);
   const [text, setText] = useState(" Enter your text here ");
   const [isBold, setIsBold] = useState(false);
@@ -38,6 +38,11 @@ export default function FormText({ heading }) {
     setIsUnderline(!isUnderline);
     console.log("text is converted to underline font");
   };
+  const handleRemoveSpaces = () => {
+    let newText = text.split(/[ ]+/).join(" ");
+    console.log("Extra spaces were removed");
+    setText(newText);
+  }
   const handleCopyClick = () => {
   navigator.clipboard.writeText(text);
   setCopied(true);
@@ -54,7 +59,7 @@ export default function FormText({ heading }) {
   return (
     <>
       <div>
-        <div className="mb-3">
+        <div className="mb-3 " style={{ color: mode === 'dark' ? 'white' : 'black'}}>
           <h2>{heading}</h2>
 
           <textarea
@@ -62,11 +67,14 @@ export default function FormText({ heading }) {
             id="myBox"
             value={text}
             rows="8"
+            
             onChange={handleOnChange}
             style={{
               fontStyle: isItalic ? "italic" : "normal",
               fontWeight: isBold ? "bold" : "normal",
               textDecoration: isUnderline ? "underline" : "normal",
+              backgroundColor: mode === 'dark' ? 'grey' : 'light',
+              color: mode=== 'light' ? 'black' : 'white', 
             }}
           ></textarea>
         </div>
@@ -86,9 +94,13 @@ export default function FormText({ heading }) {
         <button className="btn btn-primary mx-2" onClick={handleUnderlineClick}>
           Underline text{" "}
         </button>
+        <button className="btn btn-primary mx-2" onClick={handleRemoveSpaces}>
+          Remove Extra Spaces{" "}
+        </button>
          <button className="btn btn-danger mx-2" onClick={handleClearClick}>
           Clear Text{" "}
         </button>
+        
         <button className="btn btn-success mx-2" onClick={handleCopyClick}> Copy
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard" viewBox="0 0 16 16">
   <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
@@ -102,21 +114,19 @@ export default function FormText({ heading }) {
   </div>
 )}
       </div>
-      <div className="container my-3">
+      <div className="container my-3" style={{ color: mode === 'dark' ? 'white' : 'black'}} >
         <h2>Your Text Summary</h2>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").length} Minutes Read</p>
       </div>
-      <div className="container my-3">
+      <div className="container my-3" style={{ color: mode === 'dark' ? 'white' : 'black'}}>
         <h2>Preview</h2>
         {/* <p>{text}</p> */}
-        <p style={{ fontStyle: isItalic ? "italic" : "normal" }}>{text}</p>
-        <p style={{ fontWeight: isBold ? "bold" : "normal" }}>{text}</p>
-        <p style={{ textDecoration: isUnderline ? "underline" : "none" }}>
-          {text}
-        </p>
+        <p style={{ fontStyle: isItalic ? "italic" : "normal", fontWeight: isBold ? "bold" : "normal" , fontWeight: isBold ? "bold" : "normal" }}> {text.length>0?text:"Enter Text to preview"}</p>
+       
+        
       </div>
     </>
   );
