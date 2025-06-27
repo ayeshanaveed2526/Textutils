3eaff20dac432fc286e7b36955952e3f9242db86
Ayesha Naveed <hr34772@gmail.com>
2025-06-27 18:08:30 +0500

    darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 
 import "./App.css";
 import Navbar from "./Navbar";
-import  FormText from "./FormText";
+// import  FormText from "./FormText";
+import About from "./About";
 
 
 function App() {
   return (
     <>
  <Navbar/>
- <div className="container my-3">
+ {/* <div className="container my-3">
   <FormText heading="Enter the text to analyze below" />
- </div>
 
+ </div> */}
+<About/>
     </>
   );
 }
